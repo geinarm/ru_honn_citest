686cf1937ef5ea05ea97bb0ed83f4f8a2f736d8f
Guðjón Magnússon <geinarm@gmail.com>
2012-11-28 18:30:30 +0000

    Broke it some more

--- test/test_Player.cpp
+++ test/test_Player.cpp
@@ -8,7 +8,7 @@
 
 TEST( Player, checkScore ) {
     Player player;
-    EXPECT_EQ(player.getScore(), 12 );
+    EXPECT_EQ(player.getScore(), 10 );
     player.addScore();
     EXPECT_EQ(player.getScore(), 1 );
 }
